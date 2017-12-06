0f56645f3ecfb1523a05cc83c4f46f1393d0d4f7
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-06 19:03:36 +0600

    ref

--- beans/src/main/java/tech/lapsa/insurance/facade/beans/EpaymentConnectionFacadeBean.java
+++ beans/src/main/java/tech/lapsa/insurance/facade/beans/EpaymentConnectionFacadeBean.java
@@ -6,11 +6,13 @@ import java.net.URI;
 import java.time.Instant;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 
+import tech.lapsa.epayment.shared.entity.XmlPaymentCompleteUnkownTypeRequest;
 import tech.lapsa.epayment.shared.entity.XmlPaymentURISpecifierRequest;
 import tech.lapsa.epayment.shared.entity.XmlPaymentURISpecifierResponse;
-import tech.lapsa.epayment.shared.entity.XmlPaymentWithUnknwonTypeCompleterRequest;
 import tech.lapsa.epayment.shared.jms.EpaymentDestinations;
 import tech.lapsa.insurance.facade.EpaymentConnectionFacade;
 import tech.lapsa.java.commons.function.MyExceptions.IllegalArgument;
@@ -23,32 +25,45 @@ import tech.lapsa.javax.jms.client.JmsResultType;
 @Stateless
 public class EpaymentConnectionFacadeBean implements EpaymentConnectionFacade {
 
+    // READERS
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    public URI getPaymentURI(final String invoiceNumber) throws IllegalArgument, IllegalState {
+	return reThrowAsChecked(() -> _getPaymentURI(invoiceNumber));
+    }
+
+    // MODIFIERS
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public void markInvoiceHasPaid(String invoiceNumber, Double paidAmount, Instant paidInstant,
+	    String paidReference) throws IllegalArgument, IllegalState {
+	reThrowAsChecked(() -> _markInvoiceHasPaid(invoiceNumber, paidAmount, paidInstant, paidReference));
+    }
+
+    // PRIVATE
+
     @Inject
     @JmsDestination(EpaymentDestinations.SPECIFY_PAYMENT_URI)
     @JmsResultType(XmlPaymentURISpecifierResponse.class)
     private JmsCallableClient<XmlPaymentURISpecifierRequest, XmlPaymentURISpecifierResponse> paymentURISpecifierClient;
 
-    @Override
-    public URI getPaymentURI(final String invoiceNumber) throws IllegalArgument, IllegalState {
-	return reThrowAsChecked(() -> {
-	    final XmlPaymentURISpecifierRequest r = new XmlPaymentURISpecifierRequest(invoiceNumber);
-	    final XmlPaymentURISpecifierResponse resp = paymentURISpecifierClient.call(r);
-	    return resp.getURI();
-	});
+    private URI _getPaymentURI(final String invoiceNumber) {
+	final XmlPaymentURISpecifierRequest r = new XmlPaymentURISpecifierRequest(invoiceNumber);
+	final XmlPaymentURISpecifierResponse resp = paymentURISpecifierClient.call(r);
+	return resp.getURI();
     }
 
     @Inject
     @JmsDestination(EpaymentDestinations.COMPLETE_PAYMENT_WITH_UNKNOWN_TYPE)
-    @JmsResultType(XmlPaymentWithUnknwonTypeCompleterRequest.class)
-    private JmsConsumerClient<XmlPaymentWithUnknwonTypeCompleterRequest> paymentWithUnknwonTypeCompleterClient;
+    @JmsResultType(XmlPaymentCompleteUnkownTypeRequest.class)
+    private JmsConsumerClient<XmlPaymentCompleteUnkownTypeRequest> paymentWithUnknwonTypeCompleterClient;
 
-    @Override
-    public void markInvoiceHasPaid(String invoiceNumber, Double paidAmount, Instant paidInstant,
-	    String paidReference) throws IllegalArgument, IllegalState {
-	reThrowAsChecked(() -> {
-	    final XmlPaymentWithUnknwonTypeCompleterRequest r = new XmlPaymentWithUnknwonTypeCompleterRequest(
-		    invoiceNumber, paidAmount, paidInstant, paidReference);
-	    paymentWithUnknwonTypeCompleterClient.accept(r);
-	});
+    private void _markInvoiceHasPaid(String invoiceNumber, Double paidAmount, Instant paidInstant,
+	    String paidReference) {
+	final XmlPaymentCompleteUnkownTypeRequest r = new XmlPaymentCompleteUnkownTypeRequest(
+		invoiceNumber, paidAmount, paidInstant, paidReference);
+	paymentWithUnknwonTypeCompleterClient.accept(r);
     }
 }
