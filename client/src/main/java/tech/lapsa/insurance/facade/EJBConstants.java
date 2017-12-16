88ef24407e354e66c29d8afe74edc1a07c69fe87
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-16 10:51:46 +0600

    #cfrefact

--- client/src/main/java/tech/lapsa/insurance/facade/EJBConstants.java
+++ client/src/main/java/tech/lapsa/insurance/facade/EJBConstants.java
@@ -2,6 +2,6 @@ package tech.lapsa.insurance.facade;
 
 public interface EJBConstants {
 
-    public static final String APPLICATION_NAME = "insurance-facade-ear";
+    public static final String APPLICATION_NAME = "insurance-facade-app";
     public static final String MODULE_NAME = "insurance-facade";
 }
