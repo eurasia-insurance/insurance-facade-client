6d6e316a2002b72649d6e26baa9f7a589d0a09f9
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-20 14:19:38 +0600

    driven bean

---
+++ beans/src/main/java/tech/lapsa/insurance/facade/drivenBeans/InvoiceHasPaidDrivenBean.java
@@ -0,0 +1,51 @@
+package tech.lapsa.insurance.facade.drivenBeans;
+
+import java.time.Instant;
+import java.util.Currency;
+import java.util.Properties;
+
+import javax.ejb.EJB;
+import javax.ejb.MessageDriven;
+
+import tech.lapsa.epayment.shared.entity.XmlInvoiceHasPaidEvent;
+import tech.lapsa.epayment.shared.jms.EpaymentDestinations;
+import tech.lapsa.insurance.facade.InsuranceRequestFacade.InsuranceRequestFacadeRemote;
+import tech.lapsa.java.commons.exceptions.IllegalArgument;
+import tech.lapsa.java.commons.exceptions.IllegalState;
+import tech.lapsa.javax.jms.service.JmsReceiverServiceDrivenBean;
+
+@MessageDriven(mappedName = EpaymentDestinations.INVOICE_HAS_PAID)
+public class InvoiceHasPaidDrivenBean extends JmsReceiverServiceDrivenBean<XmlInvoiceHasPaidEvent> {
+
+    public InvoiceHasPaidDrivenBean() {
+	super(XmlInvoiceHasPaidEvent.class);
+    }
+
+    @Override
+    public void receiving(final XmlInvoiceHasPaidEvent entity, final Properties properties)
+	    throws IllegalArgumentException, IllegalStateException {
+	_receiving(entity, properties);
+    }
+
+    // PRIVATE
+
+    @EJB
+    private InsuranceRequestFacadeRemote insuranceRequests;
+
+    private void _receiving(final XmlInvoiceHasPaidEvent entity, final Properties properties)
+	    throws IllegalArgumentException, IllegalStateException {
+	final String methodName = entity.getMethod();
+	final Integer id = Integer.valueOf(entity.getExternalId());
+	final Instant paid = entity.getInstant();
+	final Double amount = entity.getAmount();
+	final Currency currency = entity.getCurrency();
+	final String ref = entity.getReferenceNumber();
+	try {
+	    insuranceRequests.completePayment(id, methodName, paid, amount, currency, ref);
+	} catch (IllegalArgument e) {
+	    throw e.getRuntime();
+	} catch (IllegalState e) {
+	    throw e.getRuntime();
+	}
+    }
+}
