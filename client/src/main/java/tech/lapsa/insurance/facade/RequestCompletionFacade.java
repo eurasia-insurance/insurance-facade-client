0a700a29218c6ca426bfc8a645e3701f392cef82
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-25 18:38:58 +0600

    client

---
+++ client/src/main/java/tech/lapsa/insurance/facade/RequestCompletionFacade.java
@@ -0,0 +1,43 @@
+package tech.lapsa.insurance.facade;
+
+import java.time.Instant;
+import java.util.Currency;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
+
+import com.lapsa.insurance.domain.Request;
+import com.lapsa.insurance.domain.crm.User;
+import com.lapsa.insurance.elements.TransactionProblem;
+
+import tech.lapsa.java.commons.exceptions.IllegalArgument;
+import tech.lapsa.java.commons.exceptions.IllegalState;
+
+public interface RequestCompletionFacade extends EJBConstants {
+
+    public static final String BEAN_NAME = "RequestCompletionFacadeBean";
+
+    @Local
+    public interface RequestCompletionFacadeLocal extends RequestCompletionFacade {
+    }
+
+    @Remote
+    public interface RequestCompletionFacadeRemote extends RequestCompletionFacade {
+    }
+
+    Request transactionUncomplete(Request request,
+	    User user,
+	    String note,
+	    TransactionProblem transactionProblem)
+	    throws IllegalState, IllegalArgument;
+
+    Request transactionComplete(Request request,
+	    User user,
+	    String note,
+	    String agreementNumber,
+	    Double paymentAmount,
+	    Currency paymentCurrency,
+	    Instant paymentInstant,
+	    String paymentReference) throws IllegalState, IllegalArgument;
+
+}
