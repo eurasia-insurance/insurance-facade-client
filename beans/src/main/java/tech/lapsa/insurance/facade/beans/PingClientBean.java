cbf1a8afa995c05afef30c02a0c4e9fdc9c79a20
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-01-09 11:56:25 +0600

    not injecting to catch

--- beans/src/main/java/tech/lapsa/insurance/facade/beans/PingClientBean.java
+++ beans/src/main/java/tech/lapsa/insurance/facade/beans/PingClientBean.java
@@ -1,28 +1,38 @@
 package tech.lapsa.insurance.facade.beans;
 
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 
+import tech.lapsa.esbd.dao.ESBDDAOPingService.ESBDDAOPingServiceRemote;
+import tech.lapsa.insurance.dao.InsuranceDAOPingService.InsuranceDAOPingServiceRemote;
 import tech.lapsa.insurance.facade.PingClient;
 import tech.lapsa.insurance.facade.PingClient.PingClientLocal;
 import tech.lapsa.insurance.facade.PingClient.PingClientRemote;
 import tech.lapsa.java.commons.exceptions.IllegalState;
+import tech.lapsa.java.commons.naming.MyNaming;
 
 @Stateless(name = PingClient.BEAN_NAME)
 public class PingClientBean implements PingClientLocal, PingClientRemote {
 
-    @EJB
-    private tech.lapsa.esbd.dao.PingService.PingServiceRemote esbdPing;
-
-    @EJB
-    private tech.lapsa.insurance.dao.PingService.PingServiceRemote insurancePersistencePing;
-
     @Override
     @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
     public void fullPing() throws IllegalState {
-	esbdPing.ping();
-	insurancePersistencePing.ping();
+	try {
+	    MyNaming.lookupEJB(IllegalStateException::new,
+		    ESBDDAOPingServiceRemote.APPLICATION_NAME,
+		    ESBDDAOPingServiceRemote.MODULE_NAME,
+		    ESBDDAOPingServiceRemote.BEAN_NAME,
+		    ESBDDAOPingServiceRemote.class) //
+		    .ping();
+	    MyNaming.lookupEJB(IllegalStateException::new,
+		    InsuranceDAOPingServiceRemote.APPLICATION_NAME,
+		    InsuranceDAOPingServiceRemote.MODULE_NAME,
+		    InsuranceDAOPingServiceRemote.BEAN_NAME,
+		    InsuranceDAOPingServiceRemote.class) //
+		    .ping();
+	} catch (IllegalStateException e) {
+	    throw new IllegalState(e);
+	}
     }
 }
