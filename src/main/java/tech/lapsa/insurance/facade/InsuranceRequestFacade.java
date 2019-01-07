26b2decc2aab99db6c23c34965c8fb65ba5d4e53
Vadim.Isaev <vadim.o.isaev@gmail.com>
2019-01-07 04:27:26 +0300

    facade api refactored

--- src/main/java/tech/lapsa/insurance/facade/InsuranceRequestFacade.java
+++ src/main/java/tech/lapsa/insurance/facade/InsuranceRequestFacade.java
@@ -7,10 +7,13 @@ import javax.ejb.Local;
 import javax.ejb.Remote;
 
 import com.lapsa.insurance.domain.InsuranceRequest;
+import com.lapsa.insurance.domain.crm.User;
+import com.lapsa.insurance.elements.InsuranceRequestCancellationReason;
 import com.lapsa.international.localization.LocalizationLanguage;
 import com.lapsa.international.phone.PhoneNumber;
 
 import tech.lapsa.java.commons.exceptions.IllegalArgument;
+import tech.lapsa.java.commons.exceptions.IllegalState;
 import tech.lapsa.kz.taxpayer.TaxpayerNumber;
 
 public interface InsuranceRequestFacade extends EJBConstants {
@@ -23,8 +26,9 @@ public interface InsuranceRequestFacade extends EJBConstants {
 
     @Remote
     public interface InsuranceRequestFacadeRemote extends InsuranceRequestFacade {
-
     }
+
+    <T extends InsuranceRequest> T getById(Integer id) throws IllegalState, IllegalArgument;
 
     <T extends InsuranceRequest> T newRequest(T request) throws IllegalArgument;
 
@@ -41,14 +45,30 @@ public interface InsuranceRequestFacade extends EJBConstants {
 	    Double invoiceAmount,
 	    Integer invoiceQuantity) throws IllegalArgument;
 
-    void completePayment(Integer id,
-	    String methodName,
+    <T extends InsuranceRequest> T premiumPaid(T request,
+	    String paymentMethodName,
 	    Instant paymentInstant,
-	    Double amount,
-	    Currency currency,
+	    Double paymentAmount,
+	    Currency paymentCurrency,
 	    String paymentCard,
 	    String paymentCardBank,
 	    String paymentReference,
 	    String payerName) throws IllegalArgument;
 
+    <T extends InsuranceRequest> T policyIssued(T request, User user, String agreementNumber)
+	    throws IllegalState, IllegalArgument;
+
+    <T extends InsuranceRequest> T policyIssuedAndPremiumPaid(T request,
+	    User user,
+	    String agreementNumber,
+	    String paymentMethodName,
+	    Double paymentAmount,
+	    Currency paymentCurrency,
+	    Instant paymentInstant,
+	    String paymentReference,
+	    String payerName) throws IllegalState, IllegalArgument;
+
+    <T extends InsuranceRequest> T requestCanceled(T request,
+	    User user,
+	    InsuranceRequestCancellationReason insuranceRequestCancellationReason) throws IllegalState, IllegalArgument;
 }
---
+++ src/main/java/tech/lapsa/insurance/facade/RequestFacade.java
@@ -0,0 +1,25 @@
+package tech.lapsa.insurance.facade;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
+
+import com.lapsa.insurance.domain.Request;
+import com.lapsa.insurance.domain.crm.User;
+
+import tech.lapsa.java.commons.exceptions.IllegalArgument;
+import tech.lapsa.java.commons.exceptions.IllegalState;
+
+public interface RequestFacade extends EJBConstants {
+
+    public static final String BEAN_NAME = "RequestFacadeBean";
+
+    @Local
+    public interface RequestFacadeLocal extends RequestFacade {
+    }
+
+    @Remote
+    public interface RequestFacadeRemote extends RequestFacade {
+    }
+
+    Request commentRequest(Request r, User user, String message) throws IllegalState, IllegalArgument;
+}
